6bb11db0172ea6649130703cb1caec451f7f3687
Soadul Islam <35872482+Soadul@users.noreply.github.com>
2023-11-15 10:20:31 +0600

    test upload

---
+++ src/main/java/com/example/NewEcommerce/NewEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewEcommerceApplication.class, args);
+	}
+
+}

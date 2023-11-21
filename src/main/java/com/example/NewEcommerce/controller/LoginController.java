8853fa1db1ad11a30340e2f690978ac69eb13ec1
Soadul Islam <35872482+Soadul@users.noreply.github.com>
2023-11-21 17:10:04 +0600

    Added

--- src/main/java/com/example/NewEcommerce/controller/LoginController.java
+++ src/main/java/com/example/NewEcommerce/controller/LoginController.java
@@ -4,52 +4,84 @@ import com.example.NewEcommerce.Repository.RoleRepository;
 import com.example.NewEcommerce.Repository.UserRepository;
 import com.example.NewEcommerce.model.Role;
 import com.example.NewEcommerce.model.User;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-
 public class LoginController {
-@Autowired
-private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
 
     @Autowired
-    UserRepository userRepository;
-    @Autowired
-    RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
     @GetMapping("/login")
-public String login(){
-    return "login";
+    public String login() {
+        return "login";
     }
+
     @GetMapping("/register")
-    public String registerGet(){
+    public String registerGet() {
         return "register";
     }
 
+   /* @PostMapping("/register")
+    public String registerPost(@ModelAttribute("user") User user, HttpServletRequest request) throws ServletException {
+        try {
+            String password = user.getPassword();
+            user.setPassword(password);
+            List<Role> roles = new ArrayList<>();
+            roles.add(roleRepository.findById(2).orElseThrow()); // Using orElseThrow to handle optional
+            user.setRoles(roles);
+            userRepository.save(user);
+            request.login(user.getEmail(), password);
+            return "redirect:/";
+        } catch (ServletException e) {
+            return "loginfail";
+        }
+    }*/
 
-@PostMapping("/register")
-public String registerPost(@ModelAttribute("user") User user, HttpServletRequest request) throws ServletException, IOException {
-String password = user.getPassword();
-    user.setPassword(bCryptPasswordEncoder.encode(password));
-    List<Role> roles = new ArrayList<>();
-    roles.add(roleRepository.findById(2).get());
-    user.setRoles(roles);
-    userRepository.save(user);
-    request.login(user.getEmail(),password);
-    return "redirect:/";
-}
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute("user") User user, HttpServletRequest request) {
+        String email = user.getEmail();
+
+        // Check if a user with the provided email already exists
+        if (userRepository.findByEmail(email).isPresent()) {
+            // Handle the case where the email is already registered, for example, redirect to an error page
+            return "redirect:/loginfail";
+        }
+
+        // The email is not already registered, proceed with user registration
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password)); // Assuming you have a passwordEncoder bean
+
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findById(2).orElseThrow()); // Using orElseThrow to handle optional
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        // Log in the user after successful registration
+        // request.login(email, password);
+
+        // Redirect to the home page
+        return "redirect:/shop";
+    }
 
 }
--- src/main/java/com/example/NewEcommerce/model/User.java
+++ src/main/java/com/example/NewEcommerce/model/User.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 import java.util.List;
 
 @Entity
@@ -25,26 +24,37 @@ public class User {
 
   private String lastName;
 
-  @Column(nullable = false, unique = true)
+  @Column(nullable = false, unique = true, name = "email")
   @Nonnull
   private String email;
-
 
   private String password;
 
   @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
   @JoinTable(
-      name = "user_role",
-      joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
-      inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
+          name = "user_role",
+          joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
+          inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
   private List<Role> roles;
 
   public User(User user) {
-    this.id = id;
+    this.id = user.getId();
     this.firstName = user.getFirstName();
     this.lastName = user.getLastName();
     this.email = user.getEmail();
     this.password = user.getPassword();
     this.roles = user.getRoles();
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", email='" + email + '\'' +
+            ", password='" + password + '\'' +
+            ", roles=" + roles +
+            '}';
   }
 }
